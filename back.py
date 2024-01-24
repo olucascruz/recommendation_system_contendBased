f3544e76d96b3814fbd6aa88cd7b17acabd55dd7
AA-Games-The-Dev <99156904+Adriano-Henrique-de-Souza-Andrade@users.noreply.github.com>
2024-01-24 19:33:44 -0400

    Front and fixes
    - front and fixed was made

--- back.py
+++ back.py
@@ -2,12 +2,14 @@ import pandas as pd
 
 
 def get_games():
-    df =pd.read_csv('../input/board-games/bgg_dataset.csv',sep=';',encoding='utf8')
+    df =pd.read_csv('bgg_dataset.csv',sep=';',encoding='utf8')
 
     games = {}
     columns = df.columns
     for value in df.values:
         games[value[0]] = {columns[1]:value[1], columns[2]:value[2], columns[3]:value[3], columns[4]:value[4], columns[5]:value[5], columns[6]:value[6],columns[7]:value[7], columns[8]:value[8], columns[9]:value[9]}
+
+    return games
 
 # Função para calcular a distância de Manhattan
 def manhattan(game1, game2):
@@ -21,6 +23,7 @@ def manhattan(game1, game2):
 
 # Função para recomendar música com base nas preferências do usuário
 def recommend(game_name, games):
+    games = get_games()
     userProfile = games[game_name]
     distances = []
     for game in games:
---
+++ front.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from math import sqrt
+from back import *
+
+# Função para adicionar uma nova música à MyGames
+def adicionar_game_MyGames(MyGames, nome_game, userProfile):
+    MyGames[nome_game] = userProfile
+
+# Streamlit frontend
+st.title("Jogos de tabuleiro")
+
+# Sidebar para escolher a música a ser adicionada à MyGames
+st.sidebar.title("Escolha um jogo")
+selected_game = st.sidebar.selectbox("Selecione um jogo:", list(get_games().keys()))
+
+# MyGames inicial
+MyGames = {}
+
+# Adiciona a música escolhida à MyGames
+if st.sidebar.button("Escolher jogo"):
+    adicionar_game_MyGames(MyGames, selected_game, get_games()[selected_game])
+    st.sidebar.success(f"O jogo '{selected_game}' foi escolhido!")
+
+# Atualiza a MyGames com músicas similares à última adicionada
+if MyGames:
+    st.subheader("Seu jogo:")
+    st.write(list(MyGames.keys()))
+
+    # Obtém recomendações para a última música adicionada à MyGames
+    ultima_game_adicionada = list(MyGames.keys())[-1]
+    recomendacoes = recommend(ultima_game_adicionada, get_games())
+
+    st.subheader("Jogos Baseados no jogo escolhido:")
+    st.write([f"{game} - Distância: {distancia}" for distancia, game in recomendacoes])
